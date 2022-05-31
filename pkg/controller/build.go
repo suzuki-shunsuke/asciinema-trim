52c8cb41ddfc9d1dadd908abead84f2d9ac974ca
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-31 11:07:51 +0900

    feat: support changing the playback speed

--- pkg/controller/build.go
+++ pkg/controller/build.go
@@ -20,37 +20,67 @@ func (ctrl *Controller) Build(ctx context.Context, param *Param) error {
 	return ctrl.build(ctx, param, file)
 }
 
+type State struct {
+	Prev       float64
+	ActualPrev float64
+	Speed      float64
+	Delta      float64
+}
+
+func parseLine(txt string, state *State) (string, error) {
+	if strings.HasPrefix(txt, "[") {
+		idx := strings.Index(txt, ",")
+		if idx == -1 {
+			return "", errors.New(`"," isn't found`)
+		}
+		t, err := strconv.ParseFloat(txt[1:idx], 64) //nolint:gomnd
+		if err != nil {
+			return "", fmt.Errorf("time must be float64: %w", err)
+		}
+		a := state.Prev + (t-state.ActualPrev-state.Delta)/state.Speed
+		if a <= state.Prev {
+			return "", fmt.Errorf("trimed time must be lower than interval: %w", err)
+		}
+		state.Prev = a
+		state.ActualPrev = t
+		state.Delta = 0
+		return "[" + strconv.FormatFloat(a, 'f', -1, 64) + txt[idx:], nil //nolint:gomnd
+	}
+	if strings.HasPrefix(txt, "*") {
+		s, err := strconv.ParseFloat(txt[1:], 64) //nolint:gomnd
+		if err != nil {
+			return "", fmt.Errorf("speed must be float64: %w", err)
+		}
+		if s <= 0 {
+			return "", fmt.Errorf("speed must be greater than zero: %w", err)
+		}
+		state.Speed = s
+		return "", nil
+	}
+	a, err := strconv.ParseFloat(txt, 64) //nolint:gomnd
+	if err != nil {
+		return "", fmt.Errorf("trimmed time must be float64: %w", err)
+	}
+	state.Delta = a
+	return "", nil
+}
+
 func build(write func(string), file io.Reader) error {
-	var delta float64
+	lineNum := 1
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	write(scanner.Text())
+	state := &State{
+		Speed: 1,
+	}
 	for scanner.Scan() {
+		lineNum++
 		txt := scanner.Text()
-		if strings.HasPrefix(txt, "[") {
-			idx := strings.Index(txt, ",")
-			if idx == -1 {
-				return errors.New(`"," isn't founc`)
-			}
-			t, err := strconv.ParseFloat(txt[1:idx], 64) //nolint:gomnd
-			if err != nil {
-				return fmt.Errorf("time must be float64: %w", err)
-			}
-			if delta != 0 {
-				txt = "[" + strconv.FormatFloat(t-delta, 'f', -1, 64) + txt[idx:] //nolint:gomnd
-			}
-			write(txt)
-			continue
+		if s, err := parseLine(txt, state); err != nil {
+			return fmt.Errorf("parse a line line_number=%d: %w", lineNum, err)
+		} else if s != "" {
+			write(s)
 		}
-		a, err := strconv.ParseFloat(txt, 64) //nolint:gomnd
-		if err != nil {
-			return fmt.Errorf("trimmed time must be float64: %w", err)
-		}
-		if a <= 0 {
-			return errors.New("trimmed time must be greater than 0")
-		}
-		delta += a
-		continue
 	}
 
 	if err := scanner.Err(); err != nil {
---
+++ pkg/controller/build_internal_test.go
@@ -0,0 +1,125 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_parseLine(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		name     string
+		txt      string
+		state    *State
+		isErr    bool
+		expState *State
+		exp      string
+	}{
+		{
+			name: "simple",
+			txt:  `[3.541828, "o", "Bye!"]`,
+			state: &State{
+				Speed: 1,
+			},
+			exp: `[3.541828, "o", "Bye!"]`,
+			expState: &State{
+				Speed:      1,
+				Prev:       3.541828,
+				ActualPrev: 3.541828,
+			},
+		},
+		{
+			name: "change speed",
+			txt:  `*2`,
+			state: &State{
+				Speed: 1,
+			},
+			exp: "",
+			expState: &State{
+				Speed: 2,
+			},
+		},
+		{
+			name: "trim time",
+			txt:  `1`,
+			state: &State{
+				Speed: 1,
+			},
+			exp: "",
+			expState: &State{
+				Speed: 1,
+				Delta: 1,
+			},
+		},
+		{
+			name: "trim time 2",
+			txt:  `[6.0, "o", "Bye!"]`,
+			state: &State{
+				Speed:      1,
+				Prev:       2.0,
+				ActualPrev: 3.0,
+				Delta:      1,
+			},
+			exp: `[4, "o", "Bye!"]`,
+			expState: &State{
+				Speed:      1,
+				Prev:       4.0,
+				ActualPrev: 6.0,
+			},
+		},
+		{
+			name: "change speed 2",
+			txt:  `[7.0, "o", "Bye!"]`,
+			state: &State{
+				Speed:      2,
+				Prev:       2.0,
+				ActualPrev: 3.0,
+			},
+			exp: `[4, "o", "Bye!"]`,
+			expState: &State{
+				Speed:      2,
+				Prev:       4.0,
+				ActualPrev: 7.0,
+			},
+		},
+		{
+			name: "change speed and trim",
+			txt:  `[6.0, "o", "Bye!"]`,
+			state: &State{
+				Speed:      2,
+				Prev:       2.0,
+				ActualPrev: 3.0,
+				Delta:      1,
+			},
+			exp: `[3, "o", "Bye!"]`,
+			expState: &State{
+				Speed:      2,
+				Prev:       3.0,
+				ActualPrev: 6.0,
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.name, func(t *testing.T) {
+			t.Parallel()
+			s, err := parseLine(d.txt, d.state)
+			if err != nil {
+				if d.isErr {
+					return
+				}
+				t.Fatal(err)
+			}
+			if d.isErr {
+				t.Fatal("error must be returned")
+			}
+			if s != d.exp {
+				t.Fatalf("wanted %s, got %s", d.exp, s)
+			}
+			if diff := cmp.Diff(d.state, d.expState); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
